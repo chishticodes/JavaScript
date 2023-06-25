37087688cd625a461e070abe30983525902bbfc9
chishticodes <chishticodes@gmail.com>
2023-06-25 17:01:27 +0500

    lesson four done

---
+++ lesson-04/js/app.js
@@ -0,0 +1,19 @@
+let name = 'Bilal Chishti';
+console.log(name);
+
+let age = 30;
+let city = 'Khi';
+console.log(age, city);
+
+name = 'Muhammad Bilal';
+console.log(name);
+
+//var is function scope
+var friend = 'Hasan';
+console.log(friend);
+
+//let and const are block scope
+const pi = 3.142;
+console.log(pi);
+// const can not be reassigned and const must be initialize with a value
+pi = 0.5;
