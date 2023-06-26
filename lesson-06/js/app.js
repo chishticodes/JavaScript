6a30c727f6d5eb6b28a92903bd441a73eed57d4f
chishticodes <chishticodes@gmail.com>
2023-06-26 11:42:07 +0500

    lesson six done

---
+++ lesson-06/js/app.js
@@ -0,0 +1,16 @@
+// concatination
+const firstName = 'Bilal';
+const lastName = 'Chishti';
+
+let value = firstName - lastName;
+console.log(value);
+
+//implicit type conversion
+let number1 = '10';
+let number2 = '23';
+const result1 = number1 - number2;
+console.log(result1);
+
+//instead of adding it concatinated, if even one is string js will do concatination instead of addition
+const result2 = number1 + number2;
+console.log(result2);
