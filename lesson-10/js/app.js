9671671d4fa04e551560162c5443693b90d528b8
chishticodes <chishticodes@gmail.com>
2023-06-26 16:51:35 +0500

    lesson ten done

---
+++ lesson-10/js/app.js
@@ -0,0 +1,11 @@
+const name = 'Bilal';
+let age = 30;
+
+// string concatination
+const sentence1 =
+  'hello my name is ' + name + ' and I am ' + age + ' years old';
+console.log(sentence1);
+
+// template literals
+const sentence2 = `hello my name is ${name} and I am ${age} years old`;
+console.log(sentence2);
