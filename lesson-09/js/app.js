076d248854a22bb4dee999f0b78d32dae837791e
chishticodes <chishticodes@gmail.com>
2023-06-26 16:17:13 +0500

    lesson nine done

---
+++ lesson-09/js/app.js
@@ -0,0 +1,15 @@
+//for loop
+
+console.log('for');
+for (let i = 0; i <= 5; i++) {
+  console.log('number ' + i);
+}
+
+console.log('while');
+
+//while loop
+number = 0;
+while (number <= 5) {
+  console.log('number ' + number);
+  number++;
+}
