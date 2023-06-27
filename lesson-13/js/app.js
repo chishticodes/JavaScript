490c2c4c3ad5c3dc1efae83fc616022636e9ad26
chishticodes <chishticodes@gmail.com>
2023-06-27 14:01:31 +0500

    lesson thirteen done

---
+++ lesson-13/js/app.js
@@ -0,0 +1,11 @@
+//unary operator - typeof
+let text = 'some text';
+console.log(typeof text);
+
+// binary operator
+let number = 5;
+
+// ternary operator
+// condition ? (runs if true):(runs if false)
+
+2 > 1 ? console.log('yes') : console.log('no');
