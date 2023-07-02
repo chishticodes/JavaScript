882bb9e864cde1b2d57389808ba6fe67c24b78f5
chishticodes <chishticodes@gmail.com>
2023-07-02 16:09:09 +0500

    lesson thirty four done

---
+++ lesson-34/js/app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelectorAll(`.btn`);
+
+btn.forEach(function (btn) {
+  btn.addEventListener(`click`, function (e) {
+    // console.log(e.currentTarget);
+    //   e.currentTarget.style.color = `green`;
+    console.log(`target`, e.target);
+    e.target.style.color = `green`;
+  });
+});
